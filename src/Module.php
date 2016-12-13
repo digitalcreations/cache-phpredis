ca56a809f6df8e56bd90b52d7092d3dec6c5d0b7
Vegard Andreas Larsen <vegard@digitalcreations.no>
2016-12-13 15:12:44 +0100

    Everything that can go wrong, will go wrong.

--- src/Module.php
+++ src/Module.php
@@ -7,12 +7,22 @@ namespace DC\Cache\Implementations\Redis;
  */
 class Module extends \DC\IoC\Modules\Module
 {
-    public function __construct()
+    private $connectionOrHost;
+    /**
+     * @var int
+     */
+    private $port;
+
+    public function __construct($connectionOrHost, $port = 6379)
     {
         parent::__construct("dc/cache", []);
+        $this->connectionOrHost = $connectionOrHost;
+        $this->port = $port;
     }
 
     function register(\DC\IoC\Container $container) {
-        $container->register('\DC\Cache\Implementations\Redis\Cache')->to('\DC\Cache\ICache')->withContainerLifetime();
+        $container->register(function() {
+            return new Cache($this->connectionOrHost, $this->port);
+        })->to('\DC\Cache\ICache')->withContainerLifetime();
     }
 }
